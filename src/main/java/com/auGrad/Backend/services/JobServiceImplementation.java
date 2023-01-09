9f8ceb62e9966d00d5a7b9c17b89d266630b5b54
ridhi-accolite22 <ridhi.kar@accolitedigital.com>
2023-01-09 11:55:50 +0530

    added batch name in add job

--- src/main/java/com/auGrad/Backend/services/JobServiceImplementation.java
+++ src/main/java/com/auGrad/Backend/services/JobServiceImplementation.java
@@ -1,11 +1,14 @@
 package com.auGrad.Backend.services;
 
+import com.auGrad.Backend.model.Batch;
 import com.auGrad.Backend.model.Job;
+import com.auGrad.Backend.repository.BatchRepo;
 import com.auGrad.Backend.repository.JobRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobServiceImplementation implements JobService{
@@ -14,10 +17,25 @@ public class JobServiceImplementation implements JobService{
 
     @Autowired
     private Job jobAdded;
+
+    @Autowired
+    private Batch batch;
+
+    @Autowired
+    private BatchRepo batchRepo;
     @Override
     public Job createJob(Job job){
         Job jobAdded =  jobRepo.save(job);
+        Optional<Batch> obj3 = batchRepo.findByBatchName(job.getBatchName());
 
+
+        if(obj3.isPresent()){
+            Batch b = obj3.get();
+            jobAdded.setBatchId(b.getBatchId());
+
+            // interviewAdded.setBatchId(e.getBatchN());
+        }
+     jobRepo.save(jobAdded);
         return jobAdded;
 
     }
